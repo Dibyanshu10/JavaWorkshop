4b663d2f03145ddc1cff1ce1da338f095762ca16
Dibyanshu10 <dibyanshu.singh@resilientitservices.com>
2024-06-19 07:18:43 +0530

    Added 2D array class

--- src/com/Dibyanshu/Methods/TwoDimensionalArrays.java
+++ src/com/Dibyanshu/Methods/TwoDimensionalArrays.java
@@ -6,9 +6,9 @@ public class TwoDimensionalArrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int[][] arr= new int[2][3];
-		
+
+		int[][] arr = new int[2][3];
+
 		fill2DArray(arr);
 		System.out.println("Printing the actual array :");
 		print2DArray(arr);
@@ -16,42 +16,32 @@ public class TwoDimensionalArrays {
 		System.out.println("Printing doubling the value of each element of array :");
 		twice2DArray(arr);
 		print2DArray(arr);
-	
-	}//end main
-	
-	public static void fill2DArray(int[][] arr)
-	{
+
+	}// end main
+
+	public static void fill2DArray(int[][] arr) {
 		Random random = new Random();
-		
-		
-		for(int i =0; i<arr.length; i++)
-		{
-			for(int j=0; j<arr[i].length; j++)
-			{
-				arr[i][j]= random.nextInt(100)+1;//shifting 
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = random.nextInt(100) + 1;// shifting
 			}
 		}
 	}
-	
-	public static void print2DArray(int[][] arr)
-	{
-		for(int[] ar : arr)
-		{
-			for(int num : ar)
-			{
-				System.out.print(num+" ");
+
+	public static void print2DArray(int[][] arr) {
+		for (int[] ar : arr) {
+			for (int num : ar) {
+				System.out.print(num + " ");
 			}
 			System.out.println();// to change the line after completion of inner for loop
 		}
 	}
-	
-	public static void twice2DArray(int[][] arr1)
-	{
-		for(int i =0; i<arr1.length; i++)
-		{
-			for(int j =0; j<arr1[i].length; j++)
-			{
-				arr1[i][j]=arr1[i][j]*2;
+
+	public static void twice2DArray(int[][] arr1) {
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				arr1[i][j] = arr1[i][j] * 2;
 			}
 		}
 	}
